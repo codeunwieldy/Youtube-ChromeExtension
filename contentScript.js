cddc5c3ea386675e68f71452e8a6d3c2c31e554a
Admin <jscello22@gmail.com>
2024-03-28 00:15:23 -0400

    gets the propers time and console logs it properly

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,7 @@
 (() => {
-    let youtubeLeftControls, youtubePlayer;
+    let youtubeLeftControls, youtubePlayer;  //leftc controlls is where on the DOM of a youtube page we will be inserting the bookmark btn
     let currentVideo ="";
+    let currentVideoBookmarks = {};
 
     chrome.runtime.onMessage.addListener((obj, sender, responce) => {
         const {type, value, videoId}= obj;
@@ -11,7 +12,7 @@
 
     });
 
-    const newVideoLoaded = () =>{
+    const newVideoLoaded = async () =>{
         const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];  //From css
 
         if(!bookmarkBtnExists){  // if the button does not exist on the dom it will be created
@@ -20,10 +21,29 @@
             bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
             bookmarkBtn.className = "ytp-button " + "bookmark-btn";       //styling in the css
             bookmarkBtn.title = "Click to bookmark current timestamp";   
-        }
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getsElementByClassName("video-steam")[0];
+
+            youtubeLeftControls.appendChild(bookmarkBtn);         //appends the button to the left controlls
+            bookmarkBtn.addEventListener("click",addNewBookmarkEventHandler);  
+            }
     }
 
-
+      newVideoLoaded();  //calling this function anytime the match pattern from the manifest file is met
+                        // only problem is the button is appended twice but thats fine.
+      const addNewBookmarkEventHandler = () => {
+        const currentTime = youtubePlayer.currentTime;  //current time is taken from the current time of the youtube video in seconds
+        const newBookmark = {
+            time: currentTime,
+            desc: "Bookmark at " + getTime(currentTime)  // get time function converts the seconds into time 
+        
+        };
+        console.log(newBookmark);
+        chrome.storage.sync.set({  //this is straight from chromes ducumentation
+            [currentVideo]:JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a,b)=> a.time -b.time))
+        });
+      }
     
 })();
 
